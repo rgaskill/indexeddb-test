21595b038a5a4d1e34f3181f47498fe036540be5
Roarke Gaskill <roarke.gaskill@gmail.com>
2013-03-11 19:15:34 -0500

    added basic template description

--- test/IndexedDbSpec.js
+++ test/IndexedDbSpec.js
@@ -1,6 +1,4 @@
-
-
-describe('IndexedDbSpec', function() {
+describe('DeleteDatabase', function() {
 
     window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
     var dbName = 'TestDb';
@@ -117,8 +115,82 @@ describe('IndexedDbSpec', function() {
 
     });
 
+});
+
+describe('IndexedDb Test Template', function() {
+
+    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+    var dbName = 'TestDb';
+    var db;
+
+    beforeEach(function() {
+
+        runs(function() {
+
+            var done = false;
+            var request = window.indexedDB.open(dbName);
+
+
+            request.onerror = function(event) {
+                console.log("open error", event);
+                done = true;
+            };
+
+            request.onsuccess = function(event) {
+                console.log("open success", event);
+                db = this.result;
+                done = true;
+            };
+
+            waitsFor(function() {
+                return done;
+            },5000);
+
+        });
+
+    });
+
+    afterEach(function() {
+
+        runs(function(){
+
+            db.close();
+
+            var request = window.indexedDB.deleteDatabase(dbName);
+            var done = false;
+
+            request.onerror = function(event) {
+                console.log("delete error", event);
+                done = true;
+            };
+
+            request.onsuccess = function(event) {
+                console.log("delete success", event);
+                done = true;
+            };
+
+            waitsFor(function() {
+                return done;
+            },5000);
+
+
+        });
+
+    });
+
+    it("should do some database work and be confident the db is clean at the beginning of the test.", function() {
+
+
+
+
+
+
+    });
+
+
 
 });
+
 
 
 
